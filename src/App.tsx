5bbc63358234ea6bb675c1bb803719d3cc476bf1
morcos <mdanielibra@gmail.com>
2022-08-16 18:42:43 +0200

    remove the unwanted components

--- src/App.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export const App = () => {
-  return <p>test</p>;
-};
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
-import { App } from './App';
+import Tafqit from './Tafqit';
 
-export { App };
+export default Tafqit;
