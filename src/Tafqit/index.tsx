f61e118f5f72a462ac47ccaeb6ed1b6968141aea
morcos <mdanielibra@gmail.com>
2022-08-17 01:11:36 +0200

    input of tafqit component should be string

--- src/Tafqit/index.tsx
+++ src/Tafqit/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import getTafqitFormat from '../utilities/getTafqitFormat';
 
 type Props = {
-  children: number | string;
+  children: string;
 };
 
 const Tafqit = ({ children }: Props) => {
   const tafqitFormat = getTafqitFormat(children);
+
   return <p>{tafqitFormat}</p>;
 };
 export default Tafqit;
